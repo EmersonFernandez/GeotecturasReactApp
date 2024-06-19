153befb7a30ad8a58f390696c22f497ea3a1e894
EmersonFernandez <fernandezreginoe@gmail.com>
2024-06-19 15:16:54 -0500

    Config temas

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -94,7 +94,7 @@ export default function NavBAr() {
     const changeTheme =  (isDark) => {
         const themeElement =  document.getElementById('app-theme');
         const themeBasePath = '/themes';
-        // themeElement.href = isDark ? `${themeBasePath}/${themeDark}` : `${themeBasePath}/${ThemeLight}`;
+        themeElement.href = isDark ? `${themeBasePath}/${themeDark}` : `${themeBasePath}/${ThemeLight}`;
     };
 
     useEffect(() => {
