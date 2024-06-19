479945bc139a43aa0a8982b55fc34e76647d1576
EmersonFernandez <fernandezreginoe@gmail.com>
2024-06-19 17:54:36 -0500

    Config temas

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -54,46 +54,46 @@ export default function Services() {
 
     useEffect(() => {
         setProducts([
-            {    
+            {
                 name: 'Ecosistemas Digitales',
                 description: 'Transformación Digital con metodologías de BI-AI-BIGDATA.',
                 image: logo1,
-                url:'/home'
+                url: '/home'
             },
             {
                 name: 'Gemelo Digital Smart City',
                 description: 'Transformación Digital con modelos CIM a escala de ciudad.',
                 image: logo2,
-                url:'/home'
+                url: '/home'
             },
             {
                 name: 'Gemelo Digital P.O.T.',
                 description: 'Modelo CIM con el Plan de Ordenamiento Territorial a escala de ciudad.',
                 image: logo3,
-                url:'/home'
+                url: '/home'
             },
             {
                 name: 'Gemelo Digital P.D.M',
                 description: 'Modelo CIM con el Plan de Desarrollo Municipal a escala de ciudad.',
                 image: logo4,
-                url:'/home'  
+                url: '/home'
             },
             {
                 name: 'Gemelo Digital Macroproyectos',
                 description: 'Modelo CIM de instrumentos de segundo y tercer nivel territorial',
                 image: logo5,
-                url:'/home'
+                url: '/home'
             },
             {
                 name: 'Gemelo Digital Inmobiliario',
                 description: 'Modelo CIM Proptech para el Sector Inmobiliario y la Gestión del Suelo.',
                 image: logo6,
-                url:'/home'
+                url: '/home'
             },
         ]);
     }, []);
 
-    const handleClick = (event,url) => {
+    const handleClick = (event, url) => {
         event.preventDefault(); // Previene el comportamiento por defecto del enlace
         navigate(url); // Redirige a la nueva ruta
     };
@@ -104,8 +104,8 @@ export default function Services() {
                 <img src={product.image} alt={product.name} className="shadow-2" />
                 <div className='info'>
                     <h3 className="mb-1">{product.name}</h3>
-                    <p className="mt-0 mb-3" style={{fontSize:'0.8rem'}}>{product.description}</p>
-                    <a className="btn-detalle" onClick={(e) => handleClick(e,product.url)}>Ver más</a>
+                    <p className="mt-0 mb-3" style={{ fontSize: '0.8rem' }}>{product.description}</p>
+                    <a className="btn-detalle" onClick={(e) => handleClick(e, product.url)}>Ver más</a>
                 </div>
             </div>
         );
@@ -113,12 +113,14 @@ export default function Services() {
 
     return (
         <>
-            <div className='title-servicies'>
-                <p className='h1'>NUESTROS SERVICIOS</p>
-                <p className='h2 text-center me-2 ms-2'>Ecosistemas virtuales para la Transformación Digital</p>
-            </div>
-            <div className="card mb-1 mt-1">
-                <Carousel value={products} numScroll={1} numVisible={3} responsiveOptions={responsiveOptions} itemTemplate={productTemplate} />
+            <div className='box-home'>
+                <div className='title-servicies'>
+                    <p className='h1'>NUESTROS SERVICIOS</p>
+                    <p className='h2 text-center me-2 ms-2'>Ecosistemas virtuales para la Transformación Digital</p>
+                </div>
+                <div className="card mb-1 mt-1">
+                    <Carousel value={products} numScroll={1} numVisible={3} responsiveOptions={responsiveOptions} itemTemplate={productTemplate} />
+                </div>
             </div>
         </>
     )
