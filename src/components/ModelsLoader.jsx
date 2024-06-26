c4bb6c920b7bc6767f6c55030da00546f0e89c74
EmersonFernandez <fernandezreginoe@gmail.com>
2024-06-26 12:19:56 -0500

    Peticion de carga de modelos fbx

--- src/components/ModelsLoader.jsx
+++ src/components/ModelsLoader.jsx
@@ -1,45 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
-import { useLoader } from '@react-three/fiber';
-import modelPath from '../assets/terreno.fbx';
+import { useLoader,useThree } from '@react-three/fiber';
+import * as THREE from 'three';
 
-const ModelsLoader = ({ setLoaded }) => {
+const ModelsLoader = () => {
     const [model, setModel] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+    const { scene, camera, gl } = useThree();
+
+    const fbx = useLoader(FBXLoader, `${import.meta.env.VITE_URL_API}/models/arquitectonico/2`);
 
     useEffect(() => {
-        const loader = new FBXLoader();
-        loader.load(modelPath, (fbx) => {
+        if (fbx) {
+            console.log(fbx);
+            console.log(scene);
+
+
+
+            // Manipulate the model
+            // fbx.children[2].children.forEach((el) => {
+            //     el.children[0].material.color = new THREE.Color(3, 3, 3);
+            // });
+
+            // fbx.children[1].children.forEach((el) => {
+            //     el.children[0].material.color = new THREE.Color(1.6, 1.6, 1.6);
+            // });
+
+
             setModel(fbx);
-            // Manipulación del modelo una vez cargado
-            fbx.children[2].children.forEach((el) => {
-                el.children[0].material.color.setRGB(3, 3, 3);
-            });
+        }
+    }, [fbx]);
 
-            fbx.children[1].children.forEach((el) => {
-                el.children[0].material.color.setRGB(1.6, 1.6, 1.6);
-            });
-            setIsLoading(false);
-            setLoaded(true); // Marcar como cargado cuando el modelo está listo
-        });
-
-        // Limpieza al desmontar el componente
-        return () => {
-            // loader.dispose(); // Liberar recursos del loader
-            // if (model) {
-            //     model.traverse((child) => {
-            //         if (child.isMesh) {
-            //             child.geometry.dispose(); // Liberar geometría de los meshes
-            //             child.material.dispose(); // Liberar materiales de los meshes
-            //         }
-            //     });
-            // }
-        };
-    }, []);
+    if (!model) {
+        return null; // Or a loading spinner if preferred
+    }
 
     return (
-        <group scale={[0.9, 0.9, 0.1]} rotation={[-Math.PI / 2, 0, 0]} position={[-160, -180, 150]}>
-            {model && <primitive object={model} />}
+        <group scale={[0.9, 0.9, 0.1]} rotation={[-Math.PI / 2, 0, 0]} position={[-160, -166.5, 150]}>
+            <primitive object={model} />
         </group>
     );
 };
--- src/pages/public/Models.jsx
+++ src/pages/public/Models.jsx
@@ -2,7 +2,7 @@ import React, { Suspense, useRef, useState, useEffect } from 'react';
 import ThreeScene from '../../components/ThreeScene';
 import { Canvas, useFrame } from '@react-three/fiber';
 import Sphere from '../../components/Sphere';
-import { OrbitControls, Stars, PerspectiveCamera, useProgress, Html, Sky } from '@react-three/drei';
+import { OrbitControls, Stars, PerspectiveCamera, useProgress, Html, Sky, PresentationControls ,MeshReflectorMaterial} from '@react-three/drei';
 import ModelsLoader from '../../components/ModelsLoader';
 
 export default function Models() {
@@ -29,47 +29,71 @@ export default function Models() {
         );
     }
 
-    // useEffect(() => {
-    //     if (cameraRef.current) {
-    //         cameraRef.current.rotation.set(-0.5348537447251815, 0.00902461966868703, 0.0053465637374831); // Adjust these values for desired initial rotation
-    //     }
-    // }, []);
+    useEffect(() => {
+        if (cameraRef.current) {
+            cameraRef.current.rotation.set(-Math.PI / 2, 0, 0); // Adjust these values for desired initial rotation
+        }
+    }, []);
 
     return (
         <>
             <div style={{ height: '100vh' }}>
                 <ThreeScene>
-                    <color attach="background" args={['#161c24']} />
-                    <PerspectiveCamera
-                        ref={cameraRef}
-                        position={[0, 270, 350]}
-                        fov={40}
-                        near={0.1}
-                        far={1000}
-                        makeDefault
-                    />
-                    <ambientLight intensity={1} />
-                    <pointLight position={[10, 10, 10]} />
-                    <Sky
-                        distance={450000} // Distance of the sky
-                        sunPosition={[5, 2, 8]} // Position of the sun
-                        inclination={4} // Sun inclination
-                        azimuth={2} // Sun azimuth
-                    />
+                    {/* <PresentationControls
+                        speed={0.5}
+                    // global
+                    // polar={[-0.1, Math.PI / 4]}
+                    // rotation={[Math.PI /2 , 0 , 0]}
+                    >
+ */}
 
-                    <OrbitControls
-                        enableZoom={true}
-                        zoomSpeed={3.0}
-                        minDistance={0.1} // Adjusted min distance
-                        maxDistance={1000} // Adjusted max distance
-                        enablePan={true}
+                        <color attach="background" args={['#161c24']} />
+                        <PerspectiveCamera
+                            ref={cameraRef}
+                            position={[-21.237734429428745, -3.3392068362505003, -77.47819117414106]}
+                            fov={40}
+                            near={0.1}
+                            far={1000}
+                            makeDefault
+                        />
+                        <ambientLight intensity={1} />
+                        <pointLight position={[10, 10, 10]} />
+                        <Sky
+                            distance={450000} // Distance of the sky
+                            sunPosition={[5, 2, 8]} // Position of the sun
+                            inclination={4} // Sun inclination
+                            azimuth={2} // Sun azimuth
+                        />
+
+                        <OrbitControls
+                            enableZoom={true}
+                            zoomSpeed={3.0}
+                            minDistance={0.1} // Adjusted min distance
+                            maxDistance={1000} // Adjusted max distance
+                            enablePan={true}
                         // target={[0, -100, 0]}
-                    />
+                        />
 
-                    <Suspense fallback={<Loader />}>
-                        <ModelsLoader setLoaded={setLoaded} />
-                    </Suspense>
+                        <Suspense fallback={<Loader />}>
+                            <ModelsLoader setLoaded={setLoaded} />
+                        </Suspense>
 
+                        <mesh rotation={[-Math.PI / 2, 0, 0]} position-y={-2}>
+                            <planeGeometry args={[170, 170]} />
+                            <MeshReflectorMaterial
+                                blur={[300, 100]}
+                                resolution={2048}
+                                mixBlur={1}
+                                mixStrength={40}
+                                roughness={1}
+                                depthScale={1.2}
+                                minDepthThreshold={0.4}
+                                maxDepthThreshold={1.4}
+                                color="#101010"
+                                metalness={0.5}
+                            />
+                        </mesh>
+                    {/* </PresentationControls> */}
                 </ThreeScene>
             </div>
         </>
